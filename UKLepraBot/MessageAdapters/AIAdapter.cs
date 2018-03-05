44db71ccef2e9e6f16c13d450303041abcdd8d23
Nick Sologoub <nick.sologoub@invenias.com>
2018-03-05 20:57:56 +0000

    added AI

---
+++ UKLepraBot/MessageAdapters/AIAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Chronic;
+using Microsoft.Bot.Connector;
+
+namespace UKLepraBot.MessageAdapters
+{
+    public class AIAdapter: MessageAdapterBase
+    {
+        private string[] _rubbish = new[] {".", ",", "-", "=", "#", "!", "?", "%", "@", "\"", "£", "$", "^", "&", "*", "(", ")", "_", "+", "]", "[", "{", "}", ";", ":", "~", "/", "<", ">", };
+
+        public AIAdapter(IConnectorClient connector) : base(connector)
+        {
+        }
+
+        public override async Task Process(Activity activity)
+        {
+            var messageText = activity.Text?.ToLower() ?? string.Empty;
+
+            Activity reply = null;
+
+            if (messageText.ToLower().Contains("погугли"))
+            {
+                reply = GoogleCommand(activity);
+            }
+            
+            if (reply == null) return;
+
+            await Connector.Conversations.ReplyToActivityAsync(reply);
+        }
+
+        private Activity GoogleCommand(Activity activity)
+        {
+            var messageText = activity.Text;
+
+            var activationWord = "погугли";
+
+            var cleanedMessageText = messageText;
+            _rubbish.ForEach(x => cleanedMessageText = cleanedMessageText.Replace(x, " "));
+
+            var messageParts = cleanedMessageText.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var activationWordPosition = messageParts.FindIndex(x => x.Equals(activationWord));
+            if (activationWordPosition == -1 || activationWordPosition > 3) return null;
+
+            var queryParts = messageParts.Skip(activationWordPosition+1);
+            if (!queryParts.Any()) return null;
+
+            var reply = activity.CreateReply();
+            
+            var query = String.Join("%20", queryParts);
+            reply.Text = $"http://google.co.uk/search?q={query}";
+
+            return reply;
+        }
+    }
+}
--- UKLepraBot/MessageAdapters/MessageAdapterFactory.cs
+++ UKLepraBot/MessageAdapters/MessageAdapterFactory.cs
@@ -8,7 +8,8 @@ namespace UKLepraBot.MessageAdapters
     public class MessageAdapterFactory
     {
         private readonly IConnectorClient _connectorClient;
-        public static readonly List<string> CommandAdapterActivators = new List<string> {"/status", "/huify", "/unhuify", "/uptime", "/delay", "/secret", "/reload", "/lmgtfy" };
+        public static readonly List<string> CommandAdapterActivators = new List<string> {"/status", "/huify", "/unhuify", "/uptime", "/delay", "/secret", "/reload" };
+        public static readonly List<string> AIAdapterActivators = new List<string> {"погугли" };
 
         public MessageAdapterFactory(IConnectorClient connectorClient)
         {
@@ -19,6 +20,9 @@ namespace UKLepraBot.MessageAdapters
         {
             if (HelperMethods.MentionsBot(activity) && !String.IsNullOrEmpty(activity.Text) && CommandAdapterActivators.Any(x => activity.Text.ToLower().Contains(x)))
                 return new CommandAdapter(_connectorClient);
+
+            if (!String.IsNullOrEmpty(activity.Text) && AIAdapterActivators.Any(x => activity.Text.ToLower().Contains(x)))
+                return new AIAdapter(_connectorClient);
 
             return new MessageAdapter(_connectorClient);
         }
